52330debb06f205ebf9ad62e057a2ec035cd456c
Jameel Qureshi <jameelqureshi2013@gmail.com>
2019-04-22 17:43:32 +0500

    Initial commit

---
+++ Assets/Scripts/SplashManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SplashManager : MonoBehaviour
+{
+    public Text loadingPercentage;
+
+
+
+
+    public void LoadAR()
+    {
+        StartCoroutine(LoadYourAsyncScene());
+    }
+
+    IEnumerator LoadYourAsyncScene()
+    {
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(1);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            loadingPercentage.text = (asyncLoad.progress*100)+"%";
+            yield return null;
+        }
+    }
+}
